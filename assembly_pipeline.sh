a9788f421280be7b29e3350dd0914adedcc4ed8f
markziemann <mark.ziemann@gmail.com>
2019-01-31 11:36:56 +1100

    full assembly with BFC K121 C50

--- assembly_pipeline.sh
+++ assembly_pipeline.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-
+set -x
 # application paths
-SKEWER="../app/skewer/skewer"
-FASTQC="../app/fastqc"
-ABYSSPE="../app/abyss-pe"
+SKEWER="${PWD}/../app/skewer/skewer"
+FASTQC="${PWD}/../app/fastqc"
+ABYSSPE="${PWD}/../app/abyss-pe"
+BFC="${PWD}/../app/bfc/bfc"
+k=121
+c=50
 
-k=51
-c=5
+for FQZ1 in *_R1.fq.gz ; do
 
-for FQZ1 in 100489_R1.fq.gz ; do
   FQZ2=$(echo $FQZ1 | sed 's/_R1.fq.gz/_R2.fq.gz/')
   FQT1=$(echo $FQZ1 | sed 's/.fq.gz/.fq-trimmed-pair1.fastq/')
   FQT2=$(echo $FQZ1 | sed 's/.fq.gz/.fq-trimmed-pair2.fastq/')
-  PFX=$(echo $FQZ1 | sed 's/_R1.fq.gz/')
+
+  FQC=$(echo $FQT1 | sed 's/-pair1.fastq/-combined.fastq/')
+  FQC1=$(echo $FQT1 | sed 's/-pair1.fastq/-pair1-corrected.fastq/')
+  FQC2=$(echo $FQT2 | sed 's/-pair2.fastq/-pair2-corrected.fastq/')
+
+  PFX=$(echo $FQZ1 | sed 's/_R1.fq.gz//')
 
   mkdir $PFX
   if [ ! -r $PFX/finished ] ; then
-    $FASTQC $FQZ1 $FQZ2 
-    $SKEWER -t 16 -q 20 100489_R1.fq.gz 100489_R2.fq.gz
-    $ABYSSPE -c $PFX k=$k c=$c e=$c b=0 j=$nproc in="$FQT1 $FQT2" && touch $PFX/finished
+    cd $PFX
+    ln ../$FQZ1 ../$FQZ2 .
+    $FASTQC $FQZ1 $FQZ2
+    $SKEWER -t 16 -q 20  $FQZ1 $FQZ2
+    cat $FQT1 $FQT2 > $FQC
+    $BFC  -t 16 -s 5.1m $FQC $FQT1 > $FQC1
+    $BFC  -t 16 -s 5.1m $FQC $FQT2 > $FQC2
+    $ABYSSPE  name=${PFX}  k=${k}  c=${c}  e=${c}  b=0  j=16  in="$FQC1 $FQC2" && touch finished
+    cd -
   fi
 done
 
